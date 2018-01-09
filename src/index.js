ab4596ffa2a81b91c5b526ad08adcb8142ea5eee
cj2 <cj@marucreative.com>
2018-01-09 10:08:01 -0700

    Update build

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import { unregister } from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
+// registerServiceWorker()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
 unregister() // disable until we can put more time into this
-// registerServiceWorker()
